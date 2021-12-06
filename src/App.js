9fd0ae6b26f146a91bfec99b533d0c9de18c8df2
Bronwen <bronwen.marchant@gmail.com>
2021-12-06 18:14:15 -0500

    connected firebase db

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+//components
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<h1>This is the home page</h1>}></Route>
+        <Route path="/login" element={<h1>This is the home page</h1>}></Route>
+        <Route path="/wishlist-items/:id" element={<h1>This is the home page</h1>}></Route>
+        <Route path="/wishlist-items/add" element={<h1>This is the home page</h1>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
